031571d1b8a4690720b37bf0dfc5f0fc865e0316
Soumalya Pakrashi <soumalyapakrashi@gmail.com>
2022-04-17 16:57:11 +0530

    Update URL generation

--- link-reciever.js
+++ link-reciever.js
@@ -16,6 +16,27 @@ form.addEventListener("submit", event => {
     // Set the video_id to session storage
     sessionStorage.setItem("video_id", video_id);
 
+    // Generate the URL of the next page
+    let next_page_url = "";
+
+    // If the current URL has the index.html in the last then remove it
+    if(window.location.href.endsWith("index.html") || window.location.href.endsWith("index")) {
+        next_page_url = window.location.href.split("index")[0];
+    }
+
+    // If the current URL does not have index.html in the last and has a '/', then all is set
+    else if(window.location.href.endsWith("/")) {
+        next_page_url = window.location.href;
+    }
+
+    // If the current URL does not even have a '/' in the last, then add it
+    else {
+        next_page_url = window.location.href + "/";
+    }
+
+    // Finally add the player.html in the end
+    next_page_url += "player.html";
+
     // Navigate to the video player page
-    window.location.href = "/player.html";
+    window.location.href = next_page_url;
 });
